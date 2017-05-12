26bb59f3201b200b6858bfd8564e3188006a5981
tangjijun <tangjijun@yhd.com>
2017-05-12 17:06:14 +0800

    largest number draft

--- src/LargestNumber/LargestNumber.java
+++ src/LargestNumber/LargestNumber.java
@@ -11,35 +11,47 @@ public class LargestNumber {
     String result = "";
     int[] digitCount = new int[nums.length];
     int[] hdigit = new int[nums.length];
+    int[] isuse = new int[nums.length];//1 use 0 not use
+
     for (int i = 0; i < nums.length; i++) {
       int number = nums[i];
       digitCount[i] = digitCount(number);
     }
-
     for (int i = 0; i < nums.length; i++) {
       hdigit[i] = getHdigit(nums[i]);
     }
-    List<Integer> maxDigitIndex = getMaxDigitIndexList(hdigit);
 
-    for (Integer index : maxDigitIndex) {
-      result += nums[index] + " ";
+    for (int k = 0; k < nums.length; k++) {
+
+      List<Integer> maxDigitIndex = getMaxDigitIndexList(hdigit, isuse);
+      if (maxDigitIndex.size() == 1) {
+        isuse[maxDigitIndex.get(0)] = 1;
+        result += nums[maxDigitIndex.get(0)];
+      }
     }
-
     return result;
   }
 
-  public List<Integer> getMaxDigitIndexList(int[] hdigit) {
-    List<Integer> maxhdigitIndex = new ArrayList<>();
-    maxhdigitIndex.add(0);
-    for (int i = 1; i < hdigit.length; i++) {
+  public List<Integer> getMaxDigitIndexList(int[] hdigit, int[] isuse) {
+    List<Integer> maxhdigitIndex = new ArrayList<>();//should isuse
+    for (int k = 0; k < isuse.length; k++) {
+      if (isuse[k] == 0) {
+        maxhdigitIndex.add(k);
+        break;
+      }
+    }
+
+    for (int i = maxhdigitIndex.get(0); i < hdigit.length; i++) {
+      if (isuse[i] == 1) {
+        continue;
+      }
       if (hdigit[i] > hdigit[maxhdigitIndex.get(0)]) {
         maxhdigitIndex.clear();
         maxhdigitIndex.add(i);
-      } else if (hdigit[i] == hdigit[maxhdigitIndex.get(0)]) {
+      } else if (hdigit[i] == hdigit[maxhdigitIndex.get(0)] &&maxhdigitIndex.get(0) != i){
         maxhdigitIndex.add(i);
       }
-    }
-    return maxhdigitIndex;
+    } return maxhdigitIndex;
   }
 
   public int getHdigit(int number) {
--- src/LargestNumber/LargestNumberTest.java
+++ src/LargestNumber/LargestNumberTest.java
@@ -13,10 +13,10 @@ public class LargestNumberTest {
     assertEquals(true, happyNumber.isHappy(19));
     assertEquals(false, happyNumber.isHappy(0));
     assertEquals(false, happyNumber.isHappy(Integer.MAX_VALUE));*/
-    System.out.println(largestNumber.getHdigit(0));
-    System.out.println(largestNumber.getMaxDigitIndexList(new int[] { 9, 2, 1 }));
-    System.out.println(largestNumber.largestNumber(new int[] { 3, 30, 34, 5, 9 }));
-
+    System.out.println(largestNumber.getHdigit(125));
+    System.out.println(largestNumber.getMaxDigitIndexList(new int[] { 9, 2, 1 },new int[] { 1, 0, 0 }));
+    System.out.println(largestNumber.largestNumber(new int[] { 3, 5, 9 }));
+    System.out.println(largestNumber.largestNumber(new int[] { 3, 9, 9 }));
 
   }
 }
