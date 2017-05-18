b907f5df936c5cc795e83ebaa354c0d2802e41c5
tangjijun <tangjijun@yhd.com>
2017-05-18 16:49:09 +0800

    Reverse Words in a String

--- src/ReverseWordsinaString/ReverseWordsinaStringTest.java
+++ src/ReverseWordsinaString/ReverseWordsinaStringTest.java
@@ -8,10 +8,10 @@ public class ReverseWordsinaStringTest {
 
   @Test public void testReverseWordsinaString() throws Exception {
     ReverseWordsinaString reverseWordsinaString = new ReverseWordsinaString();
-   /* assertEquals("blue is sky the", reverseWordsinaString.reverseWords("the sky is blue"));
+    assertEquals("blue is sky the", reverseWordsinaString.reverseWords("the sky is blue"));
     assertEquals("blue is sky the", reverseWordsinaString.reverseWords(" the sky is blue "));
     assertEquals("blue is sky the", reverseWordsinaString.reverseWords(" the sky is      blue "));
-    assertEquals("blue the", reverseWordsinaString.reverseWords(" the   blue "));*/
+    assertEquals("blue the", reverseWordsinaString.reverseWords(" the   blue "));
     assertEquals("a", reverseWordsinaString.reverseWords("a"));
 
   }
