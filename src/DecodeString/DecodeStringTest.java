0132b26b3821c0d052f656eb54e4642d738a31b6
tangjijun <tangjijun@yhd.com>
2017-05-19 17:44:05 +0800

    DecodeStringTest

--- src/DecodeString/DecodeStringTest.java
+++ src/DecodeString/DecodeStringTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class DecodeStringTest {
   @Test public void testDecodeString() throws Exception {
     DecodeString decodeString = new DecodeString();
-    /*assertEquals("", decodeString.decodeString(""));
+    assertEquals("", decodeString.decodeString(""));
     assertEquals("aa", decodeString.decodeString("2[a]"));
     assertEquals("aaaaaaaaaaa", decodeString.decodeString("11[a]"));
     assertEquals("ababababababababababab", decodeString.decodeString("11[ab]"));
     assertEquals("ab---ab---ab---ab---ab---ab---ab---ab---ab---ab---ab---", decodeString.decodeString("11[ab3[-]]"));
-    assertEquals("aaabcbc", decodeString.decodeString("3[a]2[bc]"));*/
+    assertEquals("aaabcbc", decodeString.decodeString("3[a]2[bc]"));
     assertEquals("abcdcdcdabcdcdcdxyxyxyxy", decodeString.decodeString("2[ab3[cd]]4[xy]"));
 
     /*Input:
